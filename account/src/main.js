fe48b6e4b85aad02167a00f39487063cc4e474dd
Jessica Moraes <jm.jeska@gmail.com>
2022-12-22 09:46:00 -0300

    Retirando conexão com o db e adicionando rotas do swagger

--- account/src/main.js
+++ account/src/main.js
@@ -1,21 +1,25 @@
 import express from 'express';
-import db from './config/dbConnect.js';
-import routes from './routes/index.js';
+import { router } from './routes.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json' assert {type: "json"}; 
 
 // Importante manter a ordem seguida aqui
 
-db.on("error", console.log.bind(console, 'Erro de conexão!'));
-db.once("open", () => {
-  console.log("Conectado ao banco com sucesso!");
-});
+
 // Criando instancia do express
 const app = express();
-app.use(express.json()); // pega o json q vem da requisição e preenche o body com ele, senão o request.body não funciona
 
-// Redirecionando rotas
-routes(app);
+app.use(express.json()); // pega o json q vem da requisição e preenche o body com ele, senão o request.body não funciona
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(router);
+
+app.get('/docs', swaggerUi.setup(swaggerDocs));
 
 app.listen(3000, function () {
-    console.log("accounts server is running");
+    console.log("Accounts server is running");
 })
+
+
+
+
 
