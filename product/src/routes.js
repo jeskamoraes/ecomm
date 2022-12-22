b132bfb23e896db0e6221a8229006384aa399a3a
Jessica Moraes <jm.jeska@gmail.com>
2022-12-22 09:48:42 -0300

    Adicionando rotas POST e GET

---
+++ product/src/routes.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const { createProduct } = require('./usecase/createProductUseCase');
+const { listProducts } = require('./usecase/listProductsUseCase');
+
+const router = Router();
+
+router.get('/products', (req, res) => {
+    listProducts()
+        .then(products => {
+            res.status(201).json(products)
+        })
+        .catch(error => {
+            res.status(400).json({ status: 'error', message: error.message });
+        });
+});
+
+router.post('/products', function (req, res) {
+
+    const product = req.body
+    createProduct(product)
+        .then((data) => {
+            res.status(201).json(data);
+        })
+        .catch((error) => {
+            res.status(400).json({ status: 'Error fetching products!', message: error.message });
+        })
+});
+
+module.exports = { router }
