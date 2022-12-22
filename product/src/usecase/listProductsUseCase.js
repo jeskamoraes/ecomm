11096db98ea3be33c5268bcf1a45b24980a1a2fe
Jessica Moraes <jm.jeska@gmail.com>
2022-12-22 09:58:10 -0300

    Retornando produtos

--- product/src/usecase/listProductsUseCase.js
+++ product/src/usecase/listProductsUseCase.js
@@ -1,8 +1,9 @@
 const { findProducts } = require("../repositories/productRepository")
 
 // Utilizando a findProducts para retornar uma lista com todos os produtos salvos
-const listProducts = () => {
-    return findProducts()
+const listProducts = async () => {
+    const products = await findProducts();
+    return products;
 }
 
 module.exports = { listProducts }
