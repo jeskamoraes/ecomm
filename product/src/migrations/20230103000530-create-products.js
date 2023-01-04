8b3a2b32adc08647e1f2c0e91b9585c7842bf757
Jessica Moraes <jm.jeska@gmail.com>
2023-01-04 11:08:34 -0300

    Adicionando migration-product através do sequelize

---
+++ product/src/migrations/20230103000530-create-products.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      value: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      featurename: {
+        type: Sequelize.STRING
+      },
+      featuredescription: {
+        type: Sequelize.STRING
+      },
+      imageurl: {
+        type: Sequelize.STRING
+      },
+      imagedescription: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
