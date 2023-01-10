14bead6f79dad175dbd6b3d80c74642da740fac8
Jessica Moraes <jm.jeska@gmail.com>
2023-01-10 14:53:40 -0300

    Adicionando features ao index, gerenciador de rotas

--- product/src/routes/index.js
+++ product/src/routes/index.js
@@ -1,12 +1,14 @@
 const bodyParser = require('body-parser')
 const product = require('./productsRoute')
 const image = require('./imagesRoute')
+const feature = require('./featuresRoute')
 
 module.exports = app => {
     app.use(
         bodyParser.json(),
         bodyParser.urlencoded({ extended: false }),
         product,
-        image
+        image,
+        feature
     )
 }
