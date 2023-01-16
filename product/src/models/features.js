85b8cf1b23e89826156826667767e1cd5ac086ba
Jessica Moraes <jm.jeska@gmail.com>
2023-01-16 20:36:49 -0300

    Atualizando para import

--- product/src/models/features.js
+++ product/src/models/features.js
@@ -1,26 +1,19 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Features extends Model {
+import { Model, DataTypes } from "sequelize";
+import client from "../repositories/databaseClient.js";
+  export class Features extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Features.belongsTo(models.Products, {
-        foreignKey: 'product_id'
-      });
+      
     }
   }
   Features.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING
   }, {
-    sequelize,
+    sequelize: client,
     modelName: 'Features',
   });
-  return Features;
-};
--- product/src/models/images.js
+++ product/src/models/images.js
@@ -1,26 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Images extends Model {
+import { Model, DataTypes } from "sequelize";
+import client from "../repositories/databaseClient.js";
+  export class Images extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Images.belongsTo(models.Products, {
-        foreignKey: 'product_id'
-      });
+      
     }
   }
   Images.init({
     url: DataTypes.STRING,
     description: DataTypes.STRING
   }, {
-    sequelize,
+    sequelize: client,
     modelName: 'Images',
   });
-  return Images;
-};
+
