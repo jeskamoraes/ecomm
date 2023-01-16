53168b41e11e693cd05a315a677756d97e91451f
Jessica Moraes <jm.jeska@gmail.com>
2023-01-16 20:39:47 -0300

    Refatorando

--- product/src/repositories/productRepository.js
+++ product/src/repositories/productRepository.js
@@ -1,60 +1,27 @@
-const Sequelize = require('sequelize');
-// Conexão com o bando de dados
-const sequelize = new Sequelize('mysql://root@localhost:3306/db_product');
-const products = require('../models/products')(sequelize, Sequelize.DataTypes);
+import { Products } from '../models/products.js'
+import { Features } from '../models/features.js';
+import { Images } from '../models/images.js';
 
-const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+export async function saveProduct(produto) {
+    const createdProduct = await Products.create(produto, {
+        include: [
+            { association: Products.Features, as: 'features',},
+            { association: Products.Images, as: 'images'}
+        ]
+    });
+    await createdProduct.save();
+    return createdProduct;
 }
 
-// const saveProduct = async product => {
-//     const createProduct = await products.create(product);
-//     return createProduct;
-// }
-
-// const findProducts = async () => {
-//     const allProducts = await products.findAll();
-//     return allProducts;
-// }
-
-module.exports = { testConnection };
-
-// Código utilizado quando desenvolvemos apenas com array e depois realizando a conexão com o mongo
-
-// const products = [];
-
-// async function getUsersCollection() {
-//     const connectionURL = 'mongodb://jessica:123456789@localhost:27017';
-//     const connection = new MongoClient(connectionURL);
-//     await connection.connect();
-  
-//     const database = connection.db('ecomm');
-//     return database.collection('products');
-//   }
-
-// Função responsável por adicionar os produtos dentro do array
-// const saveProduct = async product => {
-//     const collection = await getUsersCollection();
-//     // products.push(product)
-//     // Para retornar a lista não é necessário dar return product aqui!
-//     // Sem o return product não é retornado um produto único (primeiro teste realizado)
-//     //return product
-//     await collection.insertOne(product)
-// }
-
-// Função que não recebe nenhum parâmetro, mas retorna todos os produtos salvos
-// const findProducts = () => {
-//     const listProducts = products
-//     return listProducts;
-// }
-
-// const findProducts = async () => {
-//     const collection = await getUsersCollection();
-//     const products = collection.find().toArray();
-//     return products;
-// }
+export async function findProducts() {
+    const allProducts = await Products.findAll({ include : [
+        {
+            model: Features,
+            as: 'features'
+        }, {
+            model: Images,
+            as: 'images'
+        }]
+    });
+    return allProducts;
+}
