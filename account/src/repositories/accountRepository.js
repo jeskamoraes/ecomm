e3227d5c549173b308eb1a7bbb527cebc6dc970c
Jessica Moraes <jm.jeska@gmail.com>
2023-01-17 15:11:26 -0300

    Refatorando código da conexão com o banco

--- account/src/repositories/accountRepository.js
+++ account/src/repositories/accountRepository.js
@@ -1,37 +1,37 @@
 
 import { MongoClient } from 'mongodb';
 
-export async function getUsersCollection() {
-  const connectionURL = 'mongodb://jessica:123456789@mongodb:27017';
-  const connection = new MongoClient(connectionURL);
-  await connection.connect();
+export const client = new MongoClient('mongodb://jessica:123456789@localhost:27017');
 
-  const database = connection.db('accounts');
-  return database.collection('users');
+export function getUsersCollection(client) {
+  const db = client.db('accounts');
+  const usersCollection = db.collection('users');
+  return usersCollection;
 }
 
 export async function saveAccount(accounts) {
-  const collection = await getUsersCollection();
-  // promise
-  await collection.insertOne(accounts);
+  await client.connect();
+  const usersCollection = await getUsersCollection(client);
+  await usersCollection.insertOne(accounts);
+  await client.close();
 }
 
-export async function listAccounts() {
-  const collection = await getUsersCollection();
-  const accounts = collection.find().toArray();
+// export async function listAccounts() {
+//   const collection = await getUsersCollection();
+//   const accounts = collection.find().toArray();
 
-  return accounts;
-}
+//   return accounts;
+// }
 
-export async function findAccountByEmail(email) {
-  const collection = await getUsersCollection();
-  const account = collection.findOne({ email });
+// export async function findAccountByEmail(email) {
+//   const collection = await getUsersCollection();
+//   const account = collection.findOne({ email });
 
-  return account;
-}
+//   return account;
+// }
 
-export async function existsAccountByEmail(email) {
-  const account = await findAccountByEmail(email);
+// export async function existsAccountByEmail(email) {
+//   const account = await findAccountByEmail(email);
 
-  return account !== null;
-}
+//   return account !== null;
+// }
