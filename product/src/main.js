0ff28d139b2e0e6ecec527557975600baa5fa4ba
Jessica Moraes <jm.jeska@gmail.com>
2023-01-17 16:18:33 -0300

    Retirnado função app

--- product/src/main.js
+++ product/src/main.js
@@ -1,13 +1,10 @@
-import express from 'express';
-import { router } from './routes.js';
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocs = require('./swagger.json');
 import bodyParser from 'body-parser';
 import client from './repositories/databaseClient.js';
+import {app} from './app.js';
 
 const port = 3001;
-const app = express();
-
 // Recebe os dados recebidos via post e converte para json
 // app.use(express.json());
 app.use(bodyParser.json());
