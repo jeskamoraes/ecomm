7565589131eeda72c5d5b1494d3921adb6bbb02f
Jessica Moraes <jm.jeska@gmail.com>
2023-01-18 11:14:34 -0300

    Finalizando implantação do teste para a rota POST/products

---
+++ product/src/test/e2e/createProductUseCase.test.js
@@ -0,0 +1,37 @@
+import request from 'supertest';
+import {app} from  '../../app.js';
+import { productExample } from '../data/products.js';
+
+describe('Product Creation', () => {
+
+    it('should create a product given required product data', async () => {
+        await request(app)
+            .post('/products')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send(productExample)
+            .expect(201)
+            .expect(({ body }) => {
+                expect(body).toEqual({
+                    ...productExample,
+                    id: expect.any(Number),
+                    createdAt: expect.any(String),
+                    updatedAt: expect.any(String),
+                    features: productExample.features.map(feature => ({
+                        ...feature, 
+                        id: expect.any(Number),
+                        product_id: body.id,
+                        createdAt: expect.any(String),
+                        updatedAt: expect.any(String),
+                    })),
+                    images: productExample.images.map(images => ({
+                        ...images, 
+                        id: expect.any(Number),
+                        product_id: body.id,
+                        createdAt: expect.any(String),
+                        updatedAt: expect.any(String),
+                    }))
+                });
+            });
+    });
+});
