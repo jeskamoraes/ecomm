068d4833c84ba2d7c8e1287e239bd32154e11289
Jessica Moraes <jm.jeska@gmail.com>
2023-01-24 10:26:36 -0300

    Desenvolvendo controller de usuário

---
+++ account/src/controllers/userController.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        if(await User.findOne({ email }))
+        return res.status(400).send({ error: 'User already exists' });
+
+        const user = await User.create(req.body);
+
+        user.password = undefined;
+
+        return res.send({ user })
+    } catch (err) {
+        return res.status(400).send({ error: 'Registration failed.' })
+    }
+});
+
+router.post('/token', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).send({ error: 'User or password invalids.' });
+
+    if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'User or password invalids.' });
+
+    user.password = undefined;
+
+    const token = jwt.sign({ id: user.id }, process.env.SECRET, {
+        expiresIn: 14400,
+    });
+
+    res.send({ token });
+})
+
+module.exports = app => app.use('/accounts', router);
