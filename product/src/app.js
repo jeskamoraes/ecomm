554aa4e2a7d7051bf407dcdfa709e964774b62f4
Jessica Moraes <jm.jeska@gmail.com>
2023-01-24 22:54:49 -0300

    Chamando as rotas no app

--- product/src/app.js
+++ product/src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { router } from './routes.js';
 import bodyParser from 'body-parser';
+import { UserController } from './controllers/userController.js';
+import { ProductController } from './controllers/productController.js';
+import validationToken from './middlewares/auth.js';
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(router);
 
+const userController = new UserController();
+app.post('/register', userController.registerAccount);
+app.post('/token', userController.generateToken);
+const productController = new ProductController();
+app.post('/products/register', validationToken, productController.registerProduct);
 export {app};
