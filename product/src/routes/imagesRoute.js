c0133a327cffc07073c146cd4cb1d657210937b6
Jessica Moraes <jm.jeska@gmail.com>
2023-01-26 15:15:44 -0300

    Delete imagesRoute.js

--- product/src/routes/imagesRoute.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const ImageController = require('../controllers/ImageController')
-
-const router = Router()
-
-router.post('/images', ImageController.registerImage);
-router.get('/images', ImageController.listAllImages);
-
-module.exports = router
