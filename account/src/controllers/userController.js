7a49c503efaf45fc3403cdb8a69bfb61e89fdd2a
Jessica Moraes <jm.jeska@gmail.com>
2023-01-26 21:14:21 -0300

    Alterando o nome da variável

--- account/src/controllers/userController.js
+++ account/src/controllers/userController.js
@@ -7,9 +7,9 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
-    const verifyEmail = await User.findOne({ email });
+    const findUserByEmail = await User.findOne({ email });
     try {
-        if(verifyEmail)
+        if(findUserByEmail)
         return res.status(400).send({ error: 'User already exists' });
 
         const user = await User.create(req.body);
