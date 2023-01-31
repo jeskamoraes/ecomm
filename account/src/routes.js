c5887675f7950e8eaaa5cc70f55163b9a941ffc6
Jessica Moraes <jm.jeska@gmail.com>
2023-01-31 16:29:34 -0300

    Corrigino chamada do userValidation

--- account/src/routes.js
+++ account/src/routes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const UserController = require('./controllers/userController');
 const userValidations = require('./schemas/usersSchema');
-const { validate } = require('express-validation')
 
 module.exports = userRouter = Router();
 
 const userController = new UserController();
-userRouter.post('/register', validate(userValidations.createOrUpdateValidator), userController.registerAccount);
+userRouter.post('/register', userValidations, userController.registerAccount);
 userRouter.post('/token', userController.generateToken);
