6b1b7f7840741f1480e591363b6c4258110ca272
Jessica Moraes <jm.jeska@gmail.com>
2023-01-31 17:04:01 -0300

    Adicionando validação na rota

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { ProductController } from '../controllers/ProductController.js';
 import tokenValidation from '../middlewares/auth.js';
+import productValidations from '../schemas/productsSchema.js';
 
 const productRouter = Router();
 
 const productController = new ProductController();
-productRouter.post('/products/register', tokenValidation, productController.registerProduct);
+productRouter.post('/products/register', productValidations, tokenValidation, productController.registerProduct);
 productRouter.get('/products', productController.listAllProducts);
 
 export { productRouter };
