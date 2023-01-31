a7cc62da03fa80fba6835872b9a514ba4dfe1d78
Jessica Moraes <jm.jeska@gmail.com>
2023-01-31 16:30:22 -0300

    Adicionando Celebrate Middleware

--- account/src/app.js
+++ account/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRouter = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(express.json(),
         userRouter
-        );
+);
+
+app.use(errors());
 
 module.exports = app;
--- account/src/schemas/usersSchema.js
+++ account/src/schemas/usersSchema.js
@@ -1,13 +1,16 @@
-const Joi = require('joi');
+const { celebrate, Joi } = require('celebrate');
+const { messages } = require('joi-translation-pt-br');
 
-const userValidations = {
-    createOrUpdateValidator: {
-        body: Joi.object({
-            name: Joi.string().min(3).required(),
-            email: Joi.string().required().email(),
-            password: Joi.string().required().min(8).max(16),
-        })
-    }
+const userValidations = celebrate({
+    body: Joi.object({
+        name: Joi.string().required().min(3).max(255),
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8).max(16),
+    }),
+}, {
+    abortEarly: false,
+    messages: messages,
 }
+)
 
 module.exports = userValidations
