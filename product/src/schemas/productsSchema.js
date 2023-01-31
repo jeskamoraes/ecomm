2b7f735cbe651a8e4bb42bd33251da4479704b72
Jessica Moraes <jm.jeska@gmail.com>
2023-01-31 17:03:22 -0300

    Criando schema para validação

---
+++ product/src/schemas/productsSchema.js
@@ -0,0 +1,17 @@
+import { celebrate, Joi } from 'celebrate';
+import messages from 'joi-translation-pt-br';
+
+const productValidations = celebrate({
+        body: Joi.object({
+            name: Joi.string().required(),
+            value: Joi.number().sign('positive').required(),
+            quantity: Joi.number().sign('positive').required(),
+            category: Joi.string().required(),
+            description: Joi.string().min(200).max(500).required()
+        }),
+}, {
+    abortEarly: false,
+    messages: messages,
+})
+
+export default productValidations;
