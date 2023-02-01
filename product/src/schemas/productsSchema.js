4816f5adde09742f29b08b39f7c037f5aa928f27
Jessica Moraes <jm.jeska@gmail.com>
2023-02-01 16:52:19 -0300

    Criando validações para product

--- product/src/schemas/productsSchema.js
+++ product/src/schemas/productsSchema.js
@@ -2,13 +2,21 @@ import { celebrate, Joi } from 'celebrate';
 import messages from 'joi-translation-pt-br';
 
 const productValidations = celebrate({
-        body: Joi.object({
+    body: Joi.object({
+        name: Joi.string().required(),
+        value: Joi.number().sign('positive').required(),
+        quantity: Joi.number().sign('positive').required(),
+        category: Joi.string().required(),
+        description: Joi.string().min(200).max(500).required(),
+        features: Joi.array().min(3).items({
             name: Joi.string().required(),
-            value: Joi.number().sign('positive').required(),
-            quantity: Joi.number().sign('positive').required(),
-            category: Joi.string().required(),
-            description: Joi.string().min(200).max(500).required()
+            description: Joi.string().required()
         }),
+        images: Joi.array().min(3).items({
+            url: Joi.string().required(),
+            description: Joi.string().required()
+        }),
+    })
 }, {
     abortEarly: false,
     messages: messages,
