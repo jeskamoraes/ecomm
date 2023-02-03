31c76d218e02be4116474d3d5f48faa60b4cf975
Jessica Moraes <jm.jeska@gmail.com>
2023-02-03 20:26:54 -0300

    Finalizando validação de product com features e images

--- product/src/schemas/productsSchema.js
+++ product/src/schemas/productsSchema.js
@@ -7,7 +7,19 @@ const productValidations = celebrate({
         value: Joi.number().sign('positive').required(),
         quantity: Joi.number().sign('positive').required(),
         category: Joi.string().required(),
-        description: Joi.string().min(200).max(500).required()
+        description: Joi.string().min(200).max(500).required(),
+        features: Joi.array().min(3).required()
+            .items(Joi.object({
+                name: Joi.string().trim().required(),
+                description: Joi.string().trim().required(),
+            })
+            ),
+        images: Joi.array().min(3).required()
+            .items(Joi.object({
+                url: Joi.string().trim().required().uri({ scheme: [/https?/] }),
+                description: Joi.string().trim().required(),
+            })
+            )
     })
 }, {
     abortEarly: false,
